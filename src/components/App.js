8ec114eab6a28bd5f6e00750c10269abc70d506b
Ashley Mattam <amattam427@gmail.com>
2022-01-21 00:58:09 -0500

    commit tree 6e0227971b5338fe33f7f982f5d44f09516b5240

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
-const API = "http://localhost:3001/sushis";
+//const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushiList, setSushi] = useState([])
+  const [eatenSushi, setEatenSushi] = useState([])
+  const [wallet, setWallet] = useState(100)
+
+  useEffect(()=>{
+    fetch('http://localhost:3001/sushis')
+    .then((r)=>r.json())
+    .then((sushiData)=>{
+      setSushi(sushiData)
+    })
+  }, [])
+
+  function handleEatSushi(sushiId){
+    if(!eatenSushi.includes(sushiId) && wallet - sushiList.find((sushObj)=>sushObj.id === sushiId).price>=0) {
+    setEatenSushi((prevArr)=>[...prevArr, sushiId])
+
+    setWallet(prevWallet => prevWallet - sushiList.find((sushObj)=>sushObj.id === sushiId).price)
+    
+  } else {
+      alert('cant eat what you already ate')
+  }
+}
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer eatenSushi={eatenSushi} sushiList={sushiList} handleEatSushi={handleEatSushi} />
+      <Table wallet = {wallet} eatenSushi={eatenSushi}/>
     </div>
   );
 }
